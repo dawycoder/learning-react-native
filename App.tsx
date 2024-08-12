0bb5a7be6732734660a7ccb717f788d49c8c85cd
Dawyy I <21045081.huy@student.iuh.edu.vn>
2024-08-12 23:33:59 +0700

    How to use State

--- App.tsx
+++ App.tsx
@@ -1,14 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
+  const [count, setCount] = useState(0);
   return (
     <View style={styles.container}>
+      <Text>Welcome back !!!</Text>
+      <Text style={{ fontSize: 20, fontWeight: "600" }}>count = {count}</Text>
+
       <View>
-        <Text style={styles.parent}>Vung oi <Text style={styles.hello}>mo ra</Text></Text>
+        <Button title='Tap mee' onPress={() => setCount(count + 1)} />
+        <Button title='Reset' onPress={() => setCount(0)} color="pink" />
+
       </View>
-      <Text style={styles.hello}>Hellooooooo</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
@@ -19,14 +25,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  hello: {
-    color: "red",
-    fontSize: 30
-  },
-  parent: {
-    fontSize: 20,
-    color: "pink"
   }
-
 });
