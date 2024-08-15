b1b7827d84901bd10242cd6d127590edb01b3777
Dawyy I <21045081.huy@student.iuh.edu.vn>
2024-08-15 09:43:14 +0700

    TextInput

--- App.tsx
+++ App.tsx
@@ -1,19 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
+
+
   const [count, setCount] = useState(0);
+  const [name, setName] = useState<string>("");
+  const [age, setAge] = useState<string>("");
+
   return (
     <View style={styles.container}>
       <Text>Welcome back !!!</Text>
       <Text style={{ fontSize: 20, fontWeight: "600" }}>count = {count}</Text>
+      <Text>Your name: {name}</Text>
+      <TextInput
 
+        onChangeText={(value) => setName(value)}
+        multiline
+        style={styles.textInput} />
       <View>
         <Button title='Tap mee' onPress={() => setCount(count + 1)} />
         <Button title='Reset' onPress={() => setCount(0)} color="pink" />
+      </View>
 
+      <View>
+        <Text>
+          Your Age: {age}
+        </Text>
+        <TextInput
+          style={styles.textInput}
+          keyboardType='numeric'
+          onChangeText={(e) => setAge(e)}
+          maxLength={2} //max length of input
+        />
       </View>
     </View>
   );
@@ -25,5 +46,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textInput: {
+
+    height: 40,
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1,
   }
 });
